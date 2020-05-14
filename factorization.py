1ee4cf1f2f880361341881d76a94d3b97f489ee4
kali <hdeval1@umbc.edu>
2020-05-13 22:26:22 -0400

    fixed factorization code

--- factorization.py
+++ factorization.py
@@ -3,27 +3,30 @@ from Crypto.Util.number import long_to_bytes
 import libnum
 import sys
 import timeit
+import math
 
 # Implementation of Factorization Attack on RSA 
 # Heather DeVal, Jordan Fok, Hannah Russell 
 # CMSC 443 Final Project
 
-
-p=954354002755510667
-q=801297755486859913
-c=11287380433570645142834656528478983
+N=36391
+c1=35338
 
 def print_values():
     print("Welcome to the Factorization Attack on RSA!")
-    print("The key values used in the RSA algorithm will be printed below. Note: I only have ciphertext, and two prime number values")
-    print("The ciphertext is: ", c)
-    print("The value of one prime (q) is ", q)
-    print("The value of another prime (p) is ", p)
+    print("The key values used in the RSA algorithm will be printed below. Note: I only have ciphertext, and one prime number value - N")
+    print("The ciphertext is: ", c1)
+    print("The value of one prime (N) is ", N)
     print("**************ATTACK BEGINNING*****************")
     print()
     print()
 
 def factorTime():
+    print ("Finding factors for",N)
+    rtn=getfactor(N)
+    print ("Factors are: ",rtn)
+    p = rtn[0]
+    q = rtn[1]    
     n = p*q
     print("The product of the two primes is n: ", n)
     PHI=(p-1)*(q-1)
@@ -33,15 +36,30 @@ def factorTime():
     print("The private exponent d is determined using phi and the public exponent e (guessing 65537  - most commonly used)")
     print("The value of d is: ", d)
     print ("\n=== Attempting Decryption....... ===")
-    res=pow(c,d, n)
-    print ("The decrypted ciphertext is: %s" % ((long_to_bytes(res))))
+    res1=pow(c1,d,n)
+    print ("The decrypted ciphertext is: %s" % long_to_bytes(res1))
 
 def timing():
     print("Time to execute attack: " + str(timeit.timeit(factorTime, number=1)) +" seconds.")
 
+def getfactor(y):
+        i=0
+        while True:
+                val = y + i*i
+#               print i,val
+                sq = int(math.sqrt(val))
+                if (sq*sq == int(val)):
+                        print ("Factors: (",sq,"+",i,"),(",sq,"-",i,")")
+                        return(sq-i,sq+i)
+                i=i+1
+                if (i==10000): return("Cannot find")
+        return "Cannot find"
+
 def main():
+    value = N
     print_values()
     factorTime()
     timing()
+
 
 main()
